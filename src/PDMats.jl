0cc4a4edf1130211abea825279a8686100aeef96
Chris Binz <chris.binz@gmail.com>
2017-05-22 22:49:02 -0400

    Override chol{T<:AbstractMatrix}(...)

--- src/PDMats.jl
+++ src/PDMats.jl
@@ -45,10 +45,11 @@ module PDMats
 
     @compat abstract type AbstractPDMat{T<:Real} <: AbstractMatrix{T<:Real} end
 
-    import Base:size, getindex
+    import Base:size, getindex, chol
     size{T<:AbstractPDMat}(p::T) = size(full(p))
     getindex{T<:AbstractPDMat}(p::T, i::Int) = full(p)[i]
     getindex{T<:AbstractPDMat}(p::T, I::Vararg{Int, 2}) = getindex(full(p), I...)
+    chol{T<:AbstractPDMat}(p::T) = p.chol
 
     # source files
 
